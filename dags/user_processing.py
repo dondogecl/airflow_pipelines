490d4f0479dea6149d4fdd63e358c40ff8ca68b1
Alejandro Acevedo <ale@doges.dev>
2024-09-10 04:11:54 -0400

    added create table task to dag user_processing

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -1,1 +1,22 @@
 from airflow import DAG
+from datatime import datetime
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+# Configure the DAG
+with DAG('name', start_date=datetime(2024,1,1), schedule_interval='@daily', catchup=False) as dag:
+    
+    # task to create the user table
+    create_table = PostgresOperator(
+        task_id='create_table',
+        postgres_conn_id='postgres',
+        sql='''
+            CREATE TABLE IF NOT EXISTS users(
+                firstname TEXT NOT NULL,
+                lastname TEXT NOT NULL,
+                country TEXT NOT NULL,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL,
+                email TEXT NOT NULL
+            );
+        '''
+    )
