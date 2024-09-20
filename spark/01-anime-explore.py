3f2047f428d13525acdce061214dd2e0f0b3bc81
Alejandro Acevedo <ale@doges.dev>
2024-09-19 22:56:32 -0400

    spark/anime-explore: changed schema definition to multiline string for readibility

--- spark/01-anime-explore.py
+++ spark/01-anime-explore.py
@@ -88,13 +88,13 @@ if __name__ == "__main__":
 
 
     # define schema of data to be read
-    animes_schema = '`MAL_ID` INT, `Name` STRING, `Score` DOUBLE, `Genres` STRING, `English name` STRING,\
-        `Japanese name` STRING, `Type` STRING, `Episodes` INT, `Aired` STRING, `Premiered` STRING,\
-            `Producers` STRING, `Licensors` STRING, `Studios` STRING, `Source` STRING, `Duration` STRING,\
-                `Rating` STRING, `Ranked` DOUBLE, `Popularity` INT, `Members` INT, `Favorites` INT,\
-                    `Watching` INT, `Completed` INT, `On-Hold` INT, `Dropped` INT, `Plan to Watch` INT,\
-                         `Score-10` INT, `Score-9` INT, `Score-8` INT, `Score-7` INT, `Score-6` INT,\
-                             `Score-5` INT, `Score-4` INT, `Score-3` INT, `Score-2` INT, `Score-1` INT'
+    animes_schema = """`MAL_ID` INT, `Name` STRING, `Score` DOUBLE, `Genres` STRING, `English name` STRING,
+        `Japanese name` STRING, `Type` STRING, `Episodes` INT, `Aired` STRING, `Premiered` STRING,
+            `Producers` STRING, `Licensors` STRING, `Studios` STRING, `Source` STRING, `Duration` STRING,
+                `Rating` STRING, `Ranked` DOUBLE, `Popularity` INT, `Members` INT, `Favorites` INT,
+                    `Watching` INT, `Completed` INT, `On-Hold` INT, `Dropped` INT, `Plan to Watch` INT,
+                         `Score-10` INT, `Score-9` INT, `Score-8` INT, `Score-7` INT, `Score-6` INT,
+                             `Score-5` INT, `Score-4` INT, `Score-3` INT, `Score-2` INT, `Score-1` INT"""
     
     # read dataframe data
     df = spark.read.format('csv')\
